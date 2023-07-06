60b72f6564cc43781c85b9d06d2218f7e2e555b3
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-07-06 10:25:33 +0530

    Learning MVC_CRUD Operation using Entity Framework

---
+++ MVC_CRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_CRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+
+        }
+
+            
+    }
+}
---
+++ MVC_CRUD/Data/MVC_CRUD_DB_Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_CRUD.Models.DomainModel;
+
+namespace MVC_CRUD.Data
+{
+    public class MVC_CRUD_DB_Context : DbContext
+    {
+        public MVC_CRUD_DB_Context(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
