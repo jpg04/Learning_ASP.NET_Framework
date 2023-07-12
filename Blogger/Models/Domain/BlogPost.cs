cfbf0c2a69329e8286579e3352587dd392663393
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-07-12 20:45:22 +0530

    Started working on Blog Application

---
+++ Blogger/Models/Domain/BlogPost.cs
@@ -0,0 +1,18 @@
+﻿namespace Blogger.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public String Heading { get; set; }
+        public String PageTitle { get; set; }
+        public String Content { get; set; }
+        public String ShrotDescription { get; set; }
+        public String FeaturedImageUrl { get; set; }
+        public String UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public String Author { get; set; }
+        public bool  Visible { get; set; }
+
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
