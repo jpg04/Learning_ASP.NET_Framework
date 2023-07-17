aceb843218f0c79cca1b003dddaa623489bff778
Jaydeep Popat Godase <jaydeepgodase42161@gmail.com>
2023-07-17 12:20:08 +0530

    Created Data Folder for Database and created BloggerDbContext.cs class file for database tables

---
+++ BloggerMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BloggerMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
